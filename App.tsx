1aa3fec5f83818c8c103b156d8edc76e56a03686
Vinicius <viniciuspsilvas@gmail.com>
2023-03-02 11:56:45 +1000

    Final commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import { useFetch } from "./src/hooks/useFetch";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+import ArticleCard from "./src/components/ArticleCard";
+import ErrorMessage from "./src/components/ErrorMessage";
+import useFetch from "./src/hooks/useFetch";
+import { Articles } from "./src/types";
 
 export default function App() {
   const [text, setText] = useState("");
   const [q, setQ] = useState<string | null>(null);
-  const { data, error, isLoading } = useFetch(q);
+  const { data, error, isLoading } = useFetch<Articles>(q);
 
   const handleOnChangeText = (text: string) => {
     setText(text);
@@ -16,28 +26,43 @@ export default function App() {
     if (text && text.length > 0) setQ(text);
   };
 
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
-
   if (error) {
-    return <Text>{error}</Text>;
+    return <ErrorMessage text={error.message} />;
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Hello Sitemate!</Text>
-      <Text>Text {text}</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Hello Sitemate!</Text>
 
-      <TextInput
-        style={styles.input}
-        onChangeText={handleOnChangeText}
-        value={text}
-      />
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleOnChangeText}
+          value={text}
+          placeholder="Search for articles…"
+        />
 
-      <Button onPress={handleOnPress} title="Search" color="#841584" />
-    </View>
+        <TouchableOpacity style={styles.button} onPress={handleOnPress}>
+          <Text style={{ color: "white" }}>GO</Text>
+        </TouchableOpacity>
+      </View>
+
+      {isLoading && (
+        <Text style={{ marginVertical: 10, color: "#868686" }}>Loading...</Text>
+      )}
+
+      {!isLoading && data && (
+        <View style={{ flex: 3 }}>
+          <Text style={{ marginVertical: 10, color: "#868686" }}>Results:</Text>
+          <FlatList
+            data={data?.articles}
+            renderItem={(_renderItem) => (
+              <ArticleCard article={_renderItem.item} />
+            )}
+          />
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
 
@@ -45,11 +70,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    gap: 4,
+    marginHorizontal: 10
+  },
+  button: {
+    height: 40,
+    width: 50,
+    backgroundColor: "#009F5B",
+    justifyContent: "center",
     alignItems: "center",
-    justifyContent: "center"
+    paddingHorizontal: 10,
+    borderRadius: 8
+  },
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 4
+  },
+  header: {
+    marginTop: 40,
+    paddingVertical: 20,
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
   },
   input: {
-    backgroundColor: "#CCC",
-    width: 100
+    borderWidth: 1,
+    padding: 10,
+    height: 40,
+    borderColor: "#707070",
+    flex: 1,
+    borderRadius: 10
   }
 });
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const API_KEY = "183daca270264bad86fc5b72972fb82a";
 
@@ -8,11 +8,10 @@ interface ApiResponse<T> {
   isLoading: boolean;
 }
 
-export const useFetch = (q: string | null) => {
+const useFetch = <T,>(q: string | null): ApiResponse<T> => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
@@ -20,8 +19,8 @@ export const useFetch = (q: string | null) => {
         const response = await fetch(
           `https://newsapi.org/v2/everything?q=${q}&from=2023-02-02&sortBy=popularity&apiKey=${API_KEY}`
         );
-
         const jsonData = await response.json();
+
         setData(jsonData);
       } catch (error: any) {
         setError(error);
@@ -29,9 +28,9 @@ export const useFetch = (q: string | null) => {
         setIsLoading(false);
       }
     };
-
     if (q) fetchData();
   }, [q]);
-
   return { data, error, isLoading };
 };
+
+export default useFetch;
